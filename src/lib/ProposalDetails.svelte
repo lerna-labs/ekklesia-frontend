7e80e44d126781e65cce6cbcf7cb4688beda5e1a
Mad Orkestra <mad@madorkestra.com>
2025-10-29 17:33:20 +0100

    Threshold removed

--- src/lib/ProposalDetails.svelte
+++ src/lib/ProposalDetails.svelte
@@ -93,18 +93,4 @@
 			{/each}
 		</div>
 	{/if}
-
-	{#if proposal.thresholdReached}
-		<div class="mt-2">
-			<div class="inline rounded-xl bg-green-400 p-3 font-semibold text-green-800">
-				Threshold Reached
-			</div>
-		</div>
-	{:else if proposal.thresholdReached == false}
-		<div class="mt-2">
-			<div class="inline rounded-xl bg-red-400 px-3 py-2 font-semibold text-red-800">
-				Threshold Not Reached
-			</div>
-		</div>
-	{/if}
 </section>
